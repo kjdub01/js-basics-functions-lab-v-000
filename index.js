fe06bf0b95f0ea9c17df658946b873fbe61fbe70
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-25 04:36:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function distanceFromHqInFeet(blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function distanceTraveledInFeet() {
+function distanceTraveledInFeet(start, end) {
   
 }
