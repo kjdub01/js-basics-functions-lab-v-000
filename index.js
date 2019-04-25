5cca2e686dde74db61990772fd8e2e428d7e08e0
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-25 04:50:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function calculatesFarePrice(start, destination) {
   } else if (distance > 2000) {
     return 25;
   } else {
-    return 'cannot travel that far.';
+    return 'cannot travel that far';
   }
 }
